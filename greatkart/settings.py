e417a9f782d0b1ca54702d84ce35a8ab7acf1f12
tolgakb <tolgakabaahmet@gmail.com>
2023-04-10 17:16:02 +0100

    Product model has been created and some products has been added to the model and displayed at store.html

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'account',
+    'store',
 ]
 
 MIDDLEWARE = [
--- greatkart/views.py
+++ greatkart/views.py
@@ -2,6 +2,12 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
-    return render(request, "home.html")
+
+    products = Product.objects.all().filter(is_available = True)
+    context = {
+        'products': products
+    }
+    return render(request, "home.html", context)
